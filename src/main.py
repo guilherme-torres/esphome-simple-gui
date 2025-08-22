ef19b675c7500cb693382ba6e91fafe5da4adac7
guilherme-torres <guilhermetorres1401@gmail.com>
2025-08-22 07:04:12 -0300

    feature: add flask server

--- src/main.py
+++ src/main.py
@@ -1,12 +1,18 @@
+import os
+import subprocess
 from string import Template
 from utils import generate_password
+import yaml
+from flask import Flask, render_template
+
+# configuração básica
 
 data = {
     "device_name": "quarto",
     "platform": "ESP8266",
     "board": "nodemcuv2",
-    "wifi": {"ssid": "teste", "password": "teste123"},
-    "ota": {"password": "1234"},
+    "wifi": {"ssid": "brisa-Marka", "password": "gugu100317"},
+    "ota": {"password": ""},
 }
 
 basic_config_template = """
@@ -45,5 +51,48 @@ basic_config_yaml_str = Template(basic_config_template).substitute(
     ap_name=data["device_name"].title(), ap_password=generate_password()
 )
 
-with open(f"{data["device_name"]}.yaml", 'w') as yaml_file:
+dirname = "esphome_files"
+
+if not os.path.exists(dirname):
+    os.mkdir(dirname)
+
+file_dir = os.path.join(dirname, f"{data["device_name"]}.yaml")
+
+with open(file_dir, 'w') as yaml_file:
     yaml_file.write(basic_config_yaml_str.strip() + "\n")
+
+
+# adicionar componente a configuração
+
+component = {
+    "switch": [
+        {
+            "platform": "gpio",
+            "name": "lampada",
+            "pin": "GPIO5"
+        }
+    ]
+}
+
+def dict_to_yaml(obj):
+    return yaml.dump(obj, sort_keys=False)
+
+with open(file_dir, "a") as yaml_file:
+    yaml_file.write("\n" + dict_to_yaml(component))
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template("index.html")
+
+# process = subprocess.Popen(
+#     ["esphome", "run", file_dir, "--device", "/dev/ttyUSB0"],
+#     stdout=subprocess.PIPE,
+#     stderr=subprocess.STDOUT,
+#     text=True
+# )
+
+# for line in process.stdout:
+#     print(line, end="")
