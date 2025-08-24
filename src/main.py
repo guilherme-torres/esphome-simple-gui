16f299f64700210bbc8d0c854f2a9e691651268e
guilherme-torres <guilhermetorres1401@gmail.com>
2025-08-24 13:13:50 -0300

    feature: show esphome logs while uploading

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 import os
 import json
 import subprocess
+import threading
 from string import Template
 from src.utils import generate_password, list_serial_ports, dict_to_yaml
 from flask import Flask, redirect, render_template, request, url_for
 from flask_alembic import Alembic
+from flask_socketio import SocketIO
 from src.database.db import db
 from src.models import Device, Component
 from src.forms import SwitchGPIOForm
@@ -46,41 +48,59 @@ alembic = Alembic()
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
-app.config['SECRET_KEY'] = "super-secret-key"
+app.config["SECRET_KEY"] = "super-secret-key"
 
 db.init_app(app)
 alembic.init_app(app)
 
+socketio = SocketIO(app)
+
+
 def update_device_yaml_file(file_path: str, device_instance: Device):
     basic_config_yaml_str = Template(basic_config_template).substitute(
-        name=device_instance.name, platform=device_instance.platform, board=device_instance.board, wifi_ssid=device_instance.wifi_ssid,
-        wifi_password=device_instance.wifi_password, ota_password="" if device_instance.ota_password is None else device_instance.ota_password,
-        ap_ssid=device_instance.ap_ssid, ap_password=device_instance.ap_password
+        name=device_instance.name,
+        platform=device_instance.platform,
+        board=device_instance.board,
+        wifi_ssid=device_instance.wifi_ssid,
+        wifi_password=device_instance.wifi_password,
+        ota_password=(
+            "" if device_instance.ota_password is None else device_instance.ota_password
+        ),
+        ap_ssid=device_instance.ap_ssid,
+        ap_password=device_instance.ap_password,
     )
 
     if os.path.exists(file_path):
         os.remove(file_path)
 
-    with open(device_instance.config_file, 'w') as yaml_file:
+    with open(device_instance.config_file, "w") as yaml_file:
         yaml_file.write(basic_config_yaml_str.strip() + "\n")
 
     components = device_instance.components
     print("componentes:", components)
     if len(components) > 0:
-        switches = db.session.execute(db.select(Component).filter_by(
-            component_type="switch", device_id=device_instance.id)).scalars().all()
-        switch_dict = {
-            "switch": []
-        }
+        switches = (
+            db.session.execute(
+                db.select(Component).filter_by(
+                    component_type="switch", device_id=device_instance.id
+                )
+            )
+            .scalars()
+            .all()
+        )
+        switch_dict = {"switch": []}
         for switch_component in switches:
             config_json = json.loads(switch_component.config_json)
-            switch_dict["switch"].append({
-                "platform": switch_component.platform,
-                "name": switch_component.name,
-                **config_json,
-            })
+            switch_dict["switch"].append(
+                {
+                    "platform": switch_component.platform,
+                    "name": switch_component.name,
+                    **config_json,
+                }
+            )
         with open(device_instance.config_file, "a") as yaml_file:
             yaml_file.write("\n" + dict_to_yaml(switch_dict))
+
 
 @app.route("/create-device", methods=["POST"])
 def create_device():
@@ -90,12 +110,14 @@ def create_device():
 
     file_dir = os.path.join(dirname, f"{request.form.get("deviceName")}.yaml")
 
-    device_exist = db.session.execute(db.select(Device).filter_by(config_file=file_dir)).scalar_one_or_none()
+    device_exist = db.session.execute(
+        db.select(Device).filter_by(config_file=file_dir)
+    ).scalar_one_or_none()
 
     if device_exist:
         print("O dispositivo já existe!", device_exist)
         return redirect(url_for("list_devices"))
-    
+
     ap_ssid = f'{request.form.get("deviceName").title()} Fallback Hotspot'
     ap_password = generate_password()
 
@@ -115,14 +137,16 @@ def create_device():
     db.session.commit()
 
     update_device_yaml_file(file_path=device.config_file, device_instance=device)
-    
+
     return redirect(url_for("list_devices"))
+
 
 @app.route("/")
 def list_devices():
     devices = db.session.execute(db.select(Device)).scalars().all()
     print(devices)
     return render_template("index.html", devices=devices)
+
 
 @app.route("/delete-device/<int:device_id>", methods=["POST"])
 def delete_device(device_id):
@@ -136,6 +160,7 @@ def delete_device(device_id):
         if os.path.exists(config_file_path):
             os.remove(config_file_path)
     return redirect(url_for("list_devices"))
+
 
 @app.route("/edit-device/<int:device_id>", methods=["GET", "POST"])
 def edit_device(device_id):
@@ -152,7 +177,9 @@ def edit_device(device_id):
         old_config_file = device.config_file
         file_dir = os.path.join(dirname, f"{request.form.get("deviceName")}.yaml")
 
-        device_exist = db.session.execute(db.select(Device).filter_by(config_file=file_dir)).scalar_one_or_none()
+        device_exist = db.session.execute(
+            db.select(Device).filter_by(config_file=file_dir)
+        ).scalar_one_or_none()
 
         if device_exist:
             if device_exist.id != device_id:
@@ -174,35 +201,65 @@ def edit_device(device_id):
         print("dispositivo atualizado com sucesso no bd")
         print("atualizando arquivo de configuração do dispositivo...")
         update_device_yaml_file(file_path=old_config_file, device_instance=device)
-        
+
         return redirect(url_for("list_devices"))
     return render_template("edit-device.html", device=device, form=form)
 
-@app.route("/upload-config/<int:device_id>", methods=["POST"])
-def upload_config(device_id):
+
+@socketio.on("start_upload")
+def handle_upload(data):
+    device_id = data["device_id"]
+    serial_port = data["serial_port"]
+
+    # print(device_id, serial_port)
+
     device = db.session.get(Device, device_id)
-    if device:
-        print("fazendo upload para o dispositivo", device.config_file)
-        serial_port = request.form.get("serial_port")
-        if serial_port:
-            print("porta", serial_port)
-            process = subprocess.Popen(
-                ["esphome", "run", device.config_file, "--device", serial_port],
-                stdout=subprocess.PIPE,
-                stderr=subprocess.STDOUT,
-                text=True
-            )
-            for line in process.stdout:
-                print(line, end="")
-            return redirect(url_for("list_devices"))
-        print("porta serial não selecionada")
-        return redirect(url_for("list_devices"))
-    print("dispositivo não encontrado")
-    return redirect(url_for("list_devices"))
+    if not device:
+        socketio.emit("log", {"line": "[Erro] Dispositivo não encontrado."})
+        return
+    
+    def run_command():
+        command = ["esphome", "run", device.config_file, "--device", serial_port]
+        # command = ["ping", "-c", "5", "google.com"]
+        process = subprocess.Popen(
+            command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True
+        )
+        for line in iter(process.stdout.readline, ""):
+            socketio.emit("log", {"line": line, "device_id": device_id})
+        process.stdout.close()
+        process.wait()
+        # socketio.emit("upload_done", {"device_id": device_id})
+    
+    threading.Thread(target=run_command).start()
+
+
+# @app.route("/upload-config/<int:device_id>", methods=["POST"])
+# def upload_config(device_id):
+#     device = db.session.get(Device, device_id)
+#     if device:
+#         print("fazendo upload para o dispositivo", device.config_file)
+#         serial_port = request.form.get("serial_port")
+#         if serial_port:
+#             print("porta", serial_port)
+#             process = subprocess.Popen(
+#                 ["esphome", "run", device.config_file, "--device", serial_port],
+#                 stdout=subprocess.PIPE,
+#                 stderr=subprocess.STDOUT,
+#                 text=True
+#             )
+#             for line in process.stdout:
+#                 print(line, end="")
+#             return redirect(url_for("list_devices"))
+#         print("porta serial não selecionada")
+#         return redirect(url_for("list_devices"))
+#     print("dispositivo não encontrado")
+#     return redirect(url_for("list_devices"))
+
 
 @app.route("/available-ports")
 def list_available_ports():
     return list_serial_ports()
+
 
 @app.route("/add-component/<int:device_id>", methods=["POST"])
 def add_component(device_id):
@@ -228,6 +285,7 @@ def add_component(device_id):
     db.session.commit()
     update_device_yaml_file(file_path=device.config_file, device_instance=device)
     return redirect(url_for("edit_device", device_id=device_id))
+
 
 @app.route("/delete-component/<int:device_id>/<int:component_id>", methods=["POST"])
 def delete_component(device_id, component_id):
